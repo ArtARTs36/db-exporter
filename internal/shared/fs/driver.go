45fecfb5c6a5c42bb89ad58965335643ecfef2a4
ArtARTs36 <temicska99@mail.ru>
2024-03-13 00:41:12 +0300

    chore: add fs.Stat

--- internal/shared/fs/driver.go
+++ internal/shared/fs/driver.go
@@ -1,7 +1,16 @@
 package fs
 
+import "time"
+
 type Driver interface {
 	Exists(path string) bool
 	Mkdir(path string) error
 	CreateFile(path string, content []byte) error
+	Stat(path string) (*FileInfo, error)
+}
+
+type FileInfo struct {
+	Path      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
--- internal/shared/fs/local.go
+++ internal/shared/fs/local.go
@@ -1,6 +1,10 @@
 package fs
 
-import "os"
+import (
+	"os"
+	"syscall"
+	"time"
+)
 
 type Local struct {
 }
@@ -21,4 +25,20 @@ func (*Local) Mkdir(path string) error {
 
 func (*Local) CreateFile(path string, content []byte) error {
 	return os.WriteFile(path, content, 0755)
+}
+
+func (*Local) Stat(path string) (*FileInfo, error) {
+	stat, err := os.Stat(path)
+	if err != nil {
+		return nil, err
+	}
+
+	sysStat := stat.Sys().(*syscall.Stat_t)
+	ctime := time.Unix(sysStat.Birthtimespec.Sec, sysStat.Birthtimespec.Nsec)
+
+	return &FileInfo{
+		Path:      path,
+		CreatedAt: ctime,
+		UpdatedAt: stat.ModTime(),
+	}, nil
 }
